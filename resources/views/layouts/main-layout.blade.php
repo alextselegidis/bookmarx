1a5e72b91bf900cb8615f775667a4893e3403ce3
Alex Tselegidis <alextselegidis@gmail.com>
2025-07-03 12:41:12 +0200

    Trailing slash for proper loading in sub-folders

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -16,7 +16,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
-    <base href="{{url('')}}">
+    <base href="{{url('')}}/">
 
     <title>@yield('pageTitle') | Bookmarx</title>
 
