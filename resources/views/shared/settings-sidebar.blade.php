364a49c2eee4398ba95c29d6f06ac5bf191cf8b4
Alex Tselegidis <alextselegidis@gmail.com>
2025-07-11 16:54:16 +0200

    Sidebar style changes

--- resources/views/shared/settings-sidebar.blade.php
+++ resources/views/shared/settings-sidebar.blade.php
@@ -10,31 +10,35 @@
  * ---------------------------------------------------------------------------- */
 --}}
 
-<ul id="settings-nav" class="nav flex-column fw-bold fs-5">
+<h2 class="fs-3 mt-1 mb-3 text-muted">
+    {{__('settings')}}
+</h2>
+
+<ul id="settings-nav" class="nav flex-column fw-bold fs-5 sidebar-width">
     <li class="nav-item mb-3">
-        <a class="nav-link px-0 py-2" href="{{ route('settings') }}">
-            <i class="bi bi-gear me-2"></i>
+        <a class="nav-link px-0 py-2 d-flex align-items-center gap-3 text-primary" href="{{ route('settings') }}">
+            <i class="bi bi-gear fs-4"></i>
             {{__('general')}}
         </a>
     </li>
 
     <li class="nav-item mb-3">
-        <a class="nav-link px-0 py-2" href="{{ route('links') }}">
-            <i class="bi bi-link-45deg me-2"></i>
+        <a class="nav-link px-0 py-2 d-flex align-items-center gap-3 text-primary" href="{{ route('links') }}">
+            <i class="bi bi-link-45deg fs-4"></i>
             {{__('links')}}
         </a>
     </li>
 
     <li class="nav-item mb-3">
-        <a class="nav-link px-0 py-2" href="{{ route('tags') }}">
-            <i class="bi bi-tags me-2"></i>
+        <a class="nav-link px-0 py-2 d-flex align-items-center gap-3 text-primary" href="{{ route('tags') }}">
+            <i class="bi bi-tags fs-4"></i>
             {{__('tags')}}
         </a>
     </li>
 
     <li class="nav-item mb-3">
-        <a class="nav-link px-0 py-2" href="{{ route('users') }}">
-            <i class="bi bi-people me-2"></i>
+        <a class="nav-link px-0 py-2 d-flex align-items-center gap-3 text-primary" href="{{ route('users') }}">
+            <i class="bi bi-people fs-4"></i>
             {{__('users')}}
         </a>
     </li>
