44debd165bc27b299e2f8852367df6092849ef72
Alex Tselegidis <alextselegidis@gmail.com>
2025-07-11 16:54:28 +0200

    Add some minimal JS logic

--- public/scripts/bookmarx.js
+++ public/scripts/bookmarx.js
@@ -7,3 +7,21 @@
  * @license     https://opensource.org/licenses/GPL-3.0 - GPLv3
  * @link        https://bookmarx.org
  * ---------------------------------------------------------------------------- */
+
+// Auto-hide alert
+
+setTimeout(() => {
+    const alertNodeList = document.querySelectorAll('.alert');
+    if (alertNodeList.length) {
+        Array.from(alertNodeList).forEach((alertEl) => {
+            const bsAlert = bootstrap.Alert.getOrCreateInstance(alertEl);
+            bsAlert.close();
+        });
+    }
+}, 3000); // 3000 ms = 3 seconds
+
+// Auto-focus create modal input
+
+document
+    .getElementById('create-modal')
+    .addEventListener('shown.bs.modal', (event) => event.target.querySelector('input:not([type="hidden"])').focus());
