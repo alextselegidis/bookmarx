93039d9a7943e2d8c5c451ba4d8e0b90409d3026
Alex Tselegidis <alextselegidis@gmail.com>
2025-07-31 13:05:20 +0200

    Convert the favicon to longblob for more capacity

---
+++ database/migrations/2025_07_31_105602_modify_favicon_column_of_links_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->longText('favicon')->charset('binary')->change(); // LONGBLOB
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->binary('favicon')->change(); // BLOB
+        });
+    }
+};
