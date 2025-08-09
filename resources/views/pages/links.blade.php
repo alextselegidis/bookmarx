78760db29df470da5544a497057a48c2bd407fb0
Alex Tselegidis <alextselegidis@gmail.com>
2025-08-09 11:52:00 +0200

    Correct table styling

--- resources/views/pages/links.blade.php
+++ resources/views/pages/links.blade.php
@@ -74,7 +74,7 @@
                     @foreach($links as $link)
                         <tr onclick="window.location='{{route('links.show', $link->id)}}'" style="cursor: pointer;">
                             <td>
-                                {{Str::limit($link->title, 120)}}
+                                {{$link->title, 120}}
                             </td>
                             <td>
                                 <a href="{{$link->url}}" target="_blank">
