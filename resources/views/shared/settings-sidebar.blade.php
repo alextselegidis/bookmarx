c4f8e208f35ee51cf89377a90a8e16a5dbceeb47
Alex Tselegidis <alextselegidis@gmail.com>
2026-02-04 15:41:14 +0100

    Update the settings sidebar style

--- resources/views/shared/settings-sidebar.blade.php
+++ resources/views/shared/settings-sidebar.blade.php
@@ -13,39 +13,35 @@
 @php use App\Models\User; @endphp
 @php use App\Models\Setting; @endphp
 
-<h1 class="fs-3 mb-4 text-muted">
-    {{__('menu')}}
-</h1>
-
-<ul id="settings-nav" class="nav flex-column fw-bold fs-5 sidebar-width">
+<ul id="settings-nav" class="nav flex-column">
     @can('viewAny', Setting::class)
-        <li class="nav-item mb-3">
-            <a class="nav-link px-0 py-2 d-flex align-items-center gap-3 text-primary" href="{{ route('settings') }}">
-                <i class="bi bi-gear fs-4 text-muted"></i>
-                {{__('general')}}
+        <li class="nav-item">
+            <a class="nav-link px-0 py-2 d-flex align-items-center {{ request()->routeIs('settings') ? 'text-primary fw-medium' : 'text-body' }}" href="{{ route('settings') }}">
+                <i class="bi bi-gear me-3 text-muted"></i>
+                {{ __('settings') }}
             </a>
         </li>
     @endcan
 
-    <li class="nav-item mb-3">
-        <a class="nav-link px-0 py-2 d-flex align-items-center gap-3 text-primary" href="{{ route('links') }}">
-            <i class="bi bi-link-45deg fs-4 text-muted"></i>
-            {{__('links')}}
+    <li class="nav-item">
+        <a class="nav-link px-0 py-2 d-flex align-items-center {{ request()->routeIs('links*') ? 'text-primary fw-medium' : 'text-body' }}" href="{{ route('links') }}">
+            <i class="bi bi-link-45deg me-3 text-muted"></i>
+            {{ __('links') }}
         </a>
     </li>
 
-    <li class="nav-item mb-3">
-        <a class="nav-link px-0 py-2 d-flex align-items-center gap-3 text-primary" href="{{ route('tags') }}">
-            <i class="bi bi-tags fs-4 text-muted"></i>
-            {{__('tags')}}
+    <li class="nav-item">
+        <a class="nav-link px-0 py-2 d-flex align-items-center {{ request()->routeIs('tags*') ? 'text-primary fw-medium' : 'text-body' }}" href="{{ route('tags') }}">
+            <i class="bi bi-tags me-3 text-muted"></i>
+            {{ __('tags') }}
         </a>
     </li>
 
     @can('viewAny', User::class)
-        <li class="nav-item mb-3">
-            <a class="nav-link px-0 py-2 d-flex align-items-center gap-3 text-primary" href="{{ route('users') }}">
-                <i class="bi bi-people fs-4 text-muted"></i>
-                {{__('users')}}
+        <li class="nav-item">
+            <a class="nav-link px-0 py-2 d-flex align-items-center {{ request()->routeIs('users*') ? 'text-primary fw-medium' : 'text-body' }}" href="{{ route('users') }}">
+                <i class="bi bi-people me-3 text-muted"></i>
+                {{ __('users') }}
             </a>
         </li>
     @endcan
