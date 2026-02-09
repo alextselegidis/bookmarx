93c33c0fa9c1ab40bb4e07f012d4a0843dc2d55b
Alex Tselegidis <alextselegidis@gmail.com>
2026-02-09 18:39:20 +0100

    Save account change

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -1,5 +1,4 @@
 <?php
-
 /* ----------------------------------------------------------------------------
  * Bookmarx - Simple Bookmark Manager
  *
@@ -9,38 +8,29 @@
  * @license     https://opensource.org/licenses/GPL-3.0 - GPLv3
  * @link        https://github.com/alextselegidis/bookmarx
  * ---------------------------------------------------------------------------- */
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
 class AccountController extends Controller
 {
     public function index()
     {
         return view('pages.account');
     }
-
     public function update(Request $request)
     {
         $user = auth()->user();
-
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email,' . $user->id,
             'password' => 'nullable|min:8|confirmed',
         ]);
-
         $user->name = $validated['name'];
         $user->email = $validated['email'];
-
-        if ($validated['password']) {
+        if (!empty($validated['password'])) {
             $user->password = Hash::make($validated['password']);
         }
-
         $user->save();
-
-        return back()->with('success', 'Account updated successfully.');
+        return back()->with('success', __('record_saved_message'));
     }
 }
