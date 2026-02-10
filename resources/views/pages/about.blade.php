d6c7d92a7d3747eb9a212d3ca6b9cd3cc253c9db
Alex Tselegidis <alextselegidis@gmail.com>
2026-02-10 11:34:42 +0100

    Make buttons in about page have same width

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,26 +9,21 @@
  * @link        https://github.com/alextselegidis/bookmarx
  * ---------------------------------------------------------------------------- */
 --}}
-
 @extends('layouts.main-layout')
-
 @section('pageTitle')
     {{__('About')}}
 @endsection
-
 @section('breadcrumbs')
     @include('shared.breadcrumb', ['breadcrumbs' => [
         ['label' => __('about')]
     ]])
 @endsection
-
 @section('content')
     <div>
         <div class="mx-auto my-5 text-center" style="max-width: 600px">
             <div class="mb-5">
                 <img src="images/logo.png" alt="Logo" class="me-2" style="height: 128px">
             </div>
-
             <h1 class="fs-3 mb-5">
                 Bookmarx <span class="text-muted">v{{config('app.version')}}</span>
             </h1>
@@ -38,11 +33,11 @@
                 a clean and intuitive interface, keeping your digital collections tidy and easily retrievable.
             </div>
             <div class="d-flex gap-2 justify-content-center">
-                <a href="https://github.com/alextselegidis/bookmarx" class="btn btn-outline-primary" target="_blank">
+                <a href="https://github.com/alextselegidis/bookmarx" class="btn btn-outline-primary btn-equal-width" target="_blank" style="min-width: 180px;">
                     <i class="bi bi-github me-2"></i>
                     GitHub
                 </a>
-                <a href="https://alextselegidis.com" class="btn btn-outline-secondary" target="_blank">
+                <a href="https://alextselegidis.com" class="btn btn-outline-secondary btn-equal-width" target="_blank" style="min-width: 180px;">
                     <img src="images/alextselegidis-logo-16x16.png" alt="logo" class="me-2"/>
                     alextselegidis.com
                 </a>
@@ -50,4 +45,3 @@
         </div>
     </div>
 @endsection
-
